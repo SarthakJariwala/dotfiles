98736d2f4b680f289f01fa149009afe56704b565
SarthakJariwala <35085572+SarthakJariwala@users.noreply.github.com>
2021-08-05 18:42:41 -0700

    download miniconda after symlink

--- fresh.sh
+++ fresh.sh
@@ -22,15 +22,16 @@ brew bundle
 # Clone Github repositories
 # ./clone.sh
 
-# Install miniconda from conda-forge
-if test ! $(which conda); then
-  curl -fsSLo Miniforge3.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-$(uname -m).sh
-  /bin/bash Miniforge3.sh -b
-fi
-
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+
+# Install miniconda from conda-forge
+if test ! $(which conda); then
+  curl -fsSLo Miniforge3.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-$(uname -m).sh
+  /bin/bash Miniforge3.sh -b
+fi
+
