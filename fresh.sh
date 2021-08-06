f7eaf6ce166a69a9c238337dd09917958b04f8ff
SarthakJariwala <35085572+SarthakJariwala@users.noreply.github.com>
2021-08-06 00:48:31 -0700

    this should be working now

--- fresh.sh
+++ fresh.sh
@@ -15,32 +15,40 @@ if test ! $(which brew); then
 fi
 
 # Update Homebrew recipes
-# brew update
+brew update
 
 # Install all our dependencies with bundle (See Brewfile)
-# brew tap homebrew/bundle
-# brew bundle
+brew tap homebrew/bundle
+brew bundle
 
 # Clone Github repositories
 # ./clone.sh
 
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Symlink the Mackup config file to the home directory
+ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+
+if test $(which $SHELL) = /bin/zsh; then
+  source $HOME/.zshrc
+fi
 
 # Check for conda and install if we don't have it
 if test ! $(which conda); then
   curl -fsSLo Miniforge3.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-$(uname -m).sh
   /bin/bash Miniforge3.sh -b
   rm -rf Miniforge3.sh
+  export PATH=$HOME/anaconda/bin:$PATH
 fi
-
-export PATH=$HOME/anaconda/bin:$PATH
-
-source $HOME/.zshrc
 
 conda init "$(basename "${SHELL}")"
 
 # Install basic data science stack into default environment
 conda install --yes pandas scipy numpy matplotlib seaborn
 
+# for debugging purposes - check which python is being used to install poetry
 which python
 python -V
 
@@ -49,10 +57,3 @@ if test ! $(which poetry); then
   curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
   source $HOME/.poetry/env
 fi
-
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-# Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
