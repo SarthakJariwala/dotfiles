f0dcf6c7cc85c2c9233c1f792b64f640aa61bcae
SarthakJariwala <35085572+SarthakJariwala@users.noreply.github.com>
2021-08-06 10:27:14 -0700

    Create aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,19 @@
+# Shortcuts
+alias reloadshell="source $HOME/.zshrc"
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias c="clear"
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+
+# Git
+alias gst="git status"
+alias gb="git branch"
+alias gc="git checkout"
+alias gl="git log --oneline --decorate --color"
+alias amend="git add . && git commit --amend --no-edit"
+alias commit="git add . && git commit -m"
+alias diff="git diff"
+alias pull="git pull"
+alias push="git push"
+alias unstage="git restore --staged ."
