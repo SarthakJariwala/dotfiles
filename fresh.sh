400de00a5e4ab5768d788c1ac221f4e255fbbd0f
SarthakJariwala <35085572+SarthakJariwala@users.noreply.github.com>
2021-08-05 23:47:34 -0700

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -7,6 +7,8 @@ if test ! $(which omz); then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
+source $HOME/.zshrc
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
