5b01617010a4358844cf81478b2c95abff89b5aa
SarthakJariwala <35085572+SarthakJariwala@users.noreply.github.com>
2021-08-05 23:55:56 -0700

    update logic

--- fresh.sh
+++ fresh.sh
@@ -31,21 +31,25 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
-# Install miniconda from conda-forge
+# Check for conda and install if we don't have it
 if test ! $(which conda); then
   curl -fsSLo Miniforge3.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-$(uname -m).sh
   /bin/bash Miniforge3.sh -b
+  rm -rf Miniforge3.sh
+  
+  export PATH=$HOME/anaconda/bin:$PATH
+  
+  conda init "$(basename "${SHELL}")"
+  # Install basic data science stack into default environment
+  conda install --yes pandas scipy numpy matplotlib seaborn
 fi
-
-export PATH=$HOME/anaconda/bin:$PATH
-conda init "$(basename "${SHELL}")"
-# Install basic data science stack into default environment
-conda install --yes pandas scipy numpy matplotlib seaborn
 
 # source zshrc and then install poetry
 source $HOME/.bash_profile
 
-python -V
+which python
 
-# Install Poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+# Check for Poetry and install if we don't have it
+if test ! (which poetry); then
+  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+fi
